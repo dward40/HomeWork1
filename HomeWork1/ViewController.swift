6776d46a53980b2e45fc0f2e5854c94f2fb95989
EdStr <dadare1111@gmail.com>
2020-04-28 09:37:04 +0300

    Initial Commit

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Эдуард Нагучев on 28.04.2020.
+//  Copyright © 2020 Эдуард Нагучев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
